7b8075e68acf3d3d59a4b051084054a8b79c8195
Amjadiqbalnangri <167457336+Amjadiqbalnangri@users.noreply.github.com>
2024-04-20 23:00:27 +0400

    Created using Colab

---
+++ whileloop.py
@@ -0,0 +1,264 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyM/J9ORubAUdZCi/OVKTXsK",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/Amjadiqbalnangri/Amjadiqbalnangri/blob/main/whileloop.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "markdown",
+      "source": [
+        "# New section"
+      ],
+      "metadata": {
+        "id": "TEemaG0zG6Vk"
+      }
+    },
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "MXrRd-MTDYFn",
+        "outputId": "f4842b79-0ebf-4626-b959-613a3446637d"
+      },
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "5\n",
+            "swap:a= 5 b= 10\n",
+            "e= 20 f= 15\n"
+          ]
+        },
+        {
+          "output_type": "execute_result",
+          "data": {
+            "text/plain": [
+              "32768000000000000000"
+            ]
+          },
+          "metadata": {},
+          "execution_count": 19
+        }
+      ],
+      "source": [
+        "# Name='amjad'\n",
+        "# Class=16\n",
+        "# Age=3\n",
+        "# Expe=\"nil\"\n",
+        "# print(Class//Age)\n",
+        "# #swap two variables\n",
+        "# a=10\n",
+        "# b=5\n",
+        "# c=a\n",
+        "# a=b\n",
+        "# b=c\n",
+        "\n",
+        "# print(\"swap:a=\",a,\"b=\",b)\n",
+        "# e=15\n",
+        "# f=20\n",
+        "# e,f=f,e\n",
+        "# print(\"e=\",e,\"f=\",f)\n",
+        "# e%f\n",
+        "# e/f\n",
+        "# e*f\n",
+        "# e**f"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "# agr ham is trah ka program chahty hain\n",
+        "# 1 2 3 4 5\n",
+        "# 1 2 3 4 5\n",
+        "# 1 2 3 4 5\n",
+        "# 1 2 3 4 5\n",
+        "# 1 2 3 4 5\n",
+        "# i=1\n",
+        "# while i<=5 :\n",
+        "#   j=1\n",
+        "#   while j<=5 :\n",
+        "#     print (j, end =\" \")\n",
+        "#     j=j+1\n",
+        "#   print()\n",
+        "#   i=i+1\n",
+        "\n"
+      ],
+      "metadata": {
+        "id": "fMAXmrnEruL9"
+      },
+      "execution_count": null,
+      "outputs": []
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "# agr ham is trah ka program chahty hain\n",
+        "# 1 2 3 4 5\n",
+        "# 1 2 3 4 5\n",
+        "# 1 2 3 4 5\n",
+        "# 1 2 3 4 5\n",
+        "# 1 2 3 4 5\n",
+        "\n",
+        "\n",
+        "# i=1\n",
+        "# while i<=5 :\n",
+        "#   j=1\n",
+        "#   while j<=5 :\n",
+        "#     print (j, end =\" \")\n",
+        "#     j=j+1\n",
+        "#   print()\n",
+        "#   i=i+1"
+      ],
+      "metadata": {
+        "id": "KLAhmJtUp94k"
+      },
+      "execution_count": null,
+      "outputs": []
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "# i=1\n",
+        "# while i<=5 :\n",
+        "#   j=1\n",
+        "#   while j<=i :\n",
+        "#     print (j, end =\" \")\n",
+        "#     j=j+1\n",
+        "#   print()\n",
+        "#   i=i+1"
+      ],
+      "metadata": {
+        "id": "qrd9Bih2p_1c"
+      },
+      "execution_count": null,
+      "outputs": []
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "# agr ham is trah ka program chahty hain to\n",
+        "# 1\n",
+        "# 1 2\n",
+        "# 1 2 3\n",
+        "# 1 2 3 4\n",
+        "# 1 2 3 4 5\n",
+        "\n",
+        "# i=1\n",
+        "# while i<=5 :\n",
+        "#   j=1\n",
+        "#   while j<=i :\n",
+        "#     print (j, end =\" \")\n",
+        "#     j=j+1\n",
+        "#   print()\n",
+        "#   i=i+1\n",
+        "\n",
+        "\n",
+        "# agr ham is trah ka program chahty hain to\n",
+        "# *\n",
+        "# **\n",
+        "# ***\n",
+        "# ****\n",
+        "# *****\n",
+        "# i=1\n",
+        "# while i<=5 :\n",
+        "#   j=1\n",
+        "#   while j<=i :\n",
+        "#     print (\"*\", end =\" \")\n",
+        "#     j=j+1\n",
+        "#   print()\n",
+        "#   i=i+1"
+      ],
+      "metadata": {
+        "id": "589gM5jIuUq0"
+      },
+      "execution_count": null,
+      "outputs": []
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "# #countdown timer from user input to zero\n",
+        "# x=100\n",
+        "# x=int(input(\"enter any number\"))\n",
+        "# while x >=0 :\n",
+        "#   print (x)\n",
+        "#   x=x-1\n",
+        "# print(\"countdown done\")\n"
+      ],
+      "metadata": {
+        "id": "G8krt0pBh6S4"
+      },
+      "execution_count": null,
+      "outputs": []
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "# #sum of numbers untill a negative number from user input\n",
+        "# x=1\n",
+        "# while x>=0:\n",
+        "#   x=int(input(\"enter any number\"))\n",
+        "#   print (x)\n",
+        "#   x=x+1\n",
+        "# print(\"you entered a negative number\")\n"
+      ],
+      "metadata": {
+        "id": "Ivx0VclYlssj"
+      },
+      "execution_count": null,
+      "outputs": []
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "# original_pasword=str(\"shoib123\")\n",
+        "# pasword=\" \"\n",
+        "# while pasword != (\"shoib123\"):\n",
+        "#    pasword=(input(\"enter your pasword\"))\n",
+        "#    if pasword != (\"shoib123\"):\n",
+        "#      print(\"try again\")\n",
+        "# print(\"original_pasword\",original_pasword)\n"
+      ],
+      "metadata": {
+        "id": "Pd-uLz8brGB7"
+      },
+      "execution_count": null,
+      "outputs": []
+    },
+    {
+      "cell_type": "code",
+      "source": [],
+      "metadata": {
+        "id": "gg7CZcMPthi3"
+      },
+      "execution_count": null,
+      "outputs": []
+    }
+  ]
+}
